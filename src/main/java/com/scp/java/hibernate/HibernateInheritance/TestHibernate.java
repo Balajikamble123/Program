719f75c6366340daa9630bbe0f17c0c3a1811be1
Balajikamble123 <balajikamble144@gmail.com>
2018-12-04 14:57:26 +0530

    jggsgfgsgfsajinkt

--- src/main/java/com/scp/java/hibernate/HibernateInheritance/TestHibernate.java
+++ src/main/java/com/scp/java/hibernate/HibernateInheritance/TestHibernate.java
@@ -4,7 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-// hibernate change
+// hibernate change first
 
 public class TestHibernate {
 
