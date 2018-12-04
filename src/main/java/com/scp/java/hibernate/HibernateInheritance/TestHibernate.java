508d7dc293fa74ae5b7b9deda2a395d65c0d1a16
Balajikamble123 <balajikamble144@gmail.com>
2018-12-04 14:33:31 +0530

    2nd commit

--- src/main/java/com/scp/java/hibernate/HibernateInheritance/TestHibernate.java
+++ src/main/java/com/scp/java/hibernate/HibernateInheritance/TestHibernate.java
@@ -4,7 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-
+// hibrnate change
+//first
 public class TestHibernate {
 
 	public static void main(String[] args) {
