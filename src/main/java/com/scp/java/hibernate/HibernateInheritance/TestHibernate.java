f4c1a817da24af2e2abb65a210e447d0cba01752
Balajikamble123 <45586448+Balajikamble123@users.noreply.github.com>
2018-12-04 15:02:52 +0530

    Update TestHibernate.java

--- src/main/java/com/scp/java/hibernate/HibernateInheritance/TestHibernate.java
+++ src/main/java/com/scp/java/hibernate/HibernateInheritance/TestHibernate.java
@@ -5,7 +5,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 // hibernate change first
-
+//pulled file
 public class TestHibernate {
 
 	public static void main(String[] args) {
